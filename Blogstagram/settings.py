b1e72178744c15839ed9482d1a989c3856108a54
AzizbekUrdushov <145568746+AzizbekUrdushov@users.noreply.github.com>
2025-02-15 23:58:03 -0800

    Update settings.py

--- Blogstagram/settings.py
+++ Blogstagram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+erc=bcg-x=j*0h&%eks2$1x^d5%w4v*rao6dxa+#(g1b+-(*m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
         '127.0.0.1',  # Localhost for development
